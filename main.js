4c9c5c408cde8540eef3fd63f81ae048de34cfb6
FacelessOne4 <ryandesam@gmail.com>
2026-05-12 21:41:41 -0300

    commit

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let contador=0, qtdCliente;
 qtdCliente = Number (prompt("Quantos clientes estão cadastrados?"))
 do {
     nome[contador] = prompt("Digite seu nome");
-lanche[contador] = (prompt("Informe seu lanche"));
-bebida[contador] = (prompt("Informe sua bebida"));
+lanche[contador] = (prompt("Informe seu lanche seguindo a tabela: X-Burguer | X-Egg | Misto Quente" ));
+bebida[contador] = (prompt("Informe sua bebida: Refrigerante | Suco | Água"));
 contador++;
 } while (contador != qtdCliente);
 
